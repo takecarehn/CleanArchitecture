a16327d219cd9b18de54e2aa689daeafd55cc665
NOBITA-PC\dell_ <nobi.nobita.kid@gmail.com>
2025-04-27 16:05:17 +0700

    fix bug migrations

--- src/Application/Common/Interfaces/IApplicationDbContext.cs
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace CleanArchitecture.Application.Common.Interfaces;
+﻿using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Common.Interfaces;
 
 public interface IApplicationDbContext
 {
+    DbSet<Log> Logs { get; }
     Task<int> SaveChangesAsync(CancellationToken cancellationToken);
 }
